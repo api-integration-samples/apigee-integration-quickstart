8b669879deedc53531d4905fe1863efd74ed2fef
tyayers <tyler.ayers@gmail.com>
2024-10-10 07:55:37 +0200

    added last apigee lb part

--- 2.provision.sh
+++ 2.provision.sh
@@ -85,3 +85,106 @@ curl -X POST "https://integrations.googleapis.com/v1/projects/$PROJECT_ID/locati
 EOF
 
 echo "Application Integration status is ACTIVE"
+
+# get service attachment url
+TARGET_SERVICE=$(curl "https://apigee.googleapis.com/v1/organizations/$PROJECT_ID/instances/instance1" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" 2>/dev/null | jq --raw-output '.serviceAttachment')
+
+# create a Private Service Connect NEG that points to the service attachment
+gcloud compute network-endpoint-groups create apigee-neg \
+--network-endpoint-type=private-service-connect \
+--psc-target-service=$TARGET_SERVICE \
+--region=$REGION \
+--project=$PROJECT_ID 2>/dev/null
+
+# reserve IP address for Apigee
+gcloud compute addresses create apigee-ipaddress \
+--ip-version=IPV4 --global --project=$PROJECT_ID 2>/dev/null
+
+# store IP address
+IP_ADDRESS=$(gcloud compute addresses describe apigee-ipaddress \
+--format="get(address)" --global --project=$PROJECT_ID)
+
+# create LB backend service for the NEG
+gcloud compute backend-services create apigee-backend \
+--load-balancing-scheme=EXTERNAL_MANAGED \
+--protocol=HTTPS \
+--global --project=$PROJECT_ID 2>/dev/null 1>/dev/null
+
+# add the backend service to the NEG
+gcloud compute backend-services add-backend apigee-backend \
+--network-endpoint-group=apigee-neg \
+--network-endpoint-group-region=$REGION \
+--global --project=$PROJECT_ID 2>/dev/null 1>/dev/null
+
+# create load balancer
+gcloud compute url-maps create apigee-lb \
+--default-service=apigee-backend \
+--global --project=$PROJECT_ID 2>/dev/null 1>/dev/null
+
+# create certificate
+RUNTIME_HOST_ALIAS=$(echo "$IP_ADDRESS" | tr '.' '-').nip.io
+gcloud compute ssl-certificates create apigee-ssl-cert \
+--domains="$RUNTIME_HOST_ALIAS" --project "$PROJECT_ID" --quiet 2>/dev/null 1>/dev/null
+
+# create target HTTPS proxy
+gcloud compute target-https-proxies create apigee-proxy \
+--url-map=apigee-lb \
+--ssl-certificates=apigee-ssl-cert --project=$PROJECT_ID 2>/dev/null 1>/dev/null
+
+# create forwarding rule
+gcloud compute forwarding-rules create apigee-fw-rule \
+  --load-balancing-scheme=EXTERNAL_MANAGED \
+  --network-tier=PREMIUM \
+  --address=$IP_ADDRESS \
+  --target-https-proxy=apigee-proxy \
+  --ports=443 \
+  --global --project=$PROJECT_ID 2>/dev/null 1>/dev/null
+
+# create environment group
+curl -X POST "https://apigee.googleapis.com/v1/organizations/$PROJECT_ID/envgroups" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H 'Content-Type: application/json; charset=utf-8' \
+--data-binary @- << EOF 2>/dev/null 1>/dev/null
+
+{
+  "name": "dev",
+  "hostnames": ["$RUNTIME_HOST_ALIAS"]
+}
+EOF
+
+# create environment
+curl -X POST "https://apigee.googleapis.com/v1/organizations/$PROJECT_ID/environments" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H 'Content-Type: application/json; charset=utf-8' \
+--data-binary @- << EOF 2>/dev/null 1>/dev/null
+
+{
+  "name": "dev"
+}
+EOF
+
+# attach environment to envgroup
+curl -X POST "https://apigee.googleapis.com/v1/organizations/$PROJECT_ID/envgroups/dev/attachments" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H 'Content-Type: application/json; charset=utf-8' \
+--data-binary @- << EOF 2>/dev/null 1>/dev/null
+
+{
+  "name": "dev",
+  "environment": "dev"
+}
+EOF
+
+# attach environment to instance
+curl -X POST "https://apigee.googleapis.com/v1/organizations/$PROJECT_ID/instances/instance1/attachments" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H 'Content-Type: application/json; charset=utf-8' \
+--data-binary @- << EOF 2>/dev/null 1>/dev/null
+
+{
+  "environment": "dev"
+}
+EOF
+
+echo "Created load balancer, Apigee environment & Apigee environment group. Finished!"
