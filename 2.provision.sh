0c261ec6e3cf9b4a498230392c52ba834458d393
tyayers <tyler.ayers@gmail.com>
2024-11-14 12:30:43 +0100

    fixed bug with api hub vertex location

--- 2.provision.sh
+++ 2.provision.sh
@@ -296,7 +296,7 @@ if [ "$CREATE_APIGEE_APIHUB" == "TRUE" ]
 then
 
   # create service identity
-  gcloud beta services identity create --service=apihub.googleapis.com --project=$PROJECT_ID
+  gcloud beta services identity create --service=apihub.googleapis.com --project=$PROJECT_ID >> $LOG_FILE 2>&1
 
   # get project number and grant sa roles
   PROJECT_NUMBER=$(gcloud projects list --filter="$(gcloud config get-value project)" --format="value(PROJECT_NUMBER)")
@@ -316,16 +316,16 @@ then
   gcloud kms keys create apihub-key --keyring apihub-keyring --project=$PROJECT_ID --location $API_HUB_REGION --purpose "encryption" >> $LOG_FILE 2>&1
 
   # register host
-  curl -X POST "https://apihub.googleapis.com/v1/projects/$PROJECT_ID/locations/$API_HUB_REGION/hostProjectRegistrations?hostProjectRegistrationId=$PROJECT_ID" \
-  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
-  -H 'Content-Type: application/json; charset=utf-8' \
-  --data-binary @- << EOF >> $LOG_FILE 2>&1
+#   curl -X POST "https://apihub.googleapis.com/v1/projects/$PROJECT_ID/locations/$API_HUB_REGION/hostProjectRegistrations?hostProjectRegistrationId=$PROJECT_ID" \
+#   -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+#   -H 'Content-Type: application/json; charset=utf-8' \
+#   --data-binary @- << EOF >> $LOG_FILE 2>&1
 
-{
-  "name": "projects/$PROJECT_ID/locations/$API_HUB_REGION/hostProjectRegistrations/$PROJECT_ID",
-  "gcpProject": "projects/$PROJECT_ID"
-}
-EOF
+# {
+#   "name": "projects/$PROJECT_ID/locations/$API_HUB_REGION/hostProjectRegistrations/$PROJECT_ID",
+#   "gcpProject": "projects/$PROJECT_ID"
+# }
+# EOF
 
   APIHUB_RESULT=$(curl -X POST "https://apihub.googleapis.com/v1/projects/$PROJECT_ID/locations/$API_HUB_REGION/apiHubInstances?apiHubInstanceId=apihub1" \
   -H "Authorization: Bearer $(gcloud auth print-access-token)" \
@@ -335,11 +335,12 @@ EOF
 {
   "name": "projects/$PROJECT_ID/locations/$API_HUB_REGION/apiHubInstances/apihub1",
   "config": {
-    "cmekKeyName": "projects/$PROJECT_ID/locations/$API_HUB_REGION/keyRings/apihub-keyring/cryptoKeys/apihub-key"
+    "cmekKeyName": "projects/$PROJECT_ID/locations/$API_HUB_REGION/keyRings/apihub-keyring/cryptoKeys/apihub-key",
+    "vertexLocation": "$API_HUB_REGION"
   }
 }
 EOF
-  )
+)
 
   echo "API Hub status is ACTIVE"
 fi
